ae0899b06c83f0fa4411377eec2162c359f80340
xpirxt <apirat.ithx@gmail.com>
2024-12-25 15:33:37 +0700

    Add class-components.js file
    Add class-components.js file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from "react";
 import { Content, Footer, Header } from "./func-components";
+import Calendar from "./class-components";
 
 
 function App() {
   return (
-    [<Header />, <Content />, <Footer />]
+    //[<Header />, <Content />, <Footer />, <Calendar />]
+    <>
+      <Header />
+      <p><center><Calendar /></center></p>
+      <Content />
+      <Footer />
+    </>
   )
 }
 

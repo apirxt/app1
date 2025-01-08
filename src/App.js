dfed6a600651e993279dbbe84ffcac9f6e162254
xpirxt <apirat.ithx@gmail.com>
2025-01-08 15:40:05 +0700

    Add event-data.js
    Add event-data.js

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import React from "react";
 import { Content, Footer, Header } from "./func-components";
 import Button from "./class-components";
 import { Calculator2 } from "./calculator";
+import { EventData2 } from "./event-data";
 
 function App() {
-  return <Calculator2 />
+  return <EventData2 />
 }
 export default App

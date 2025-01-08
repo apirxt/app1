d90251faceb96f1c670a5885585ea640562fdf4c
xpirxt <apirat.ithx@gmail.com>
2025-01-08 15:03:38 +0700

    Add Calculator.js
    Add Calculator.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from "react";
 import { Content, Footer, Header } from "./func-components";
 import Button from "./class-components";
+import { Calculator2 } from "./calculator";
 
 function App() {
-  return <Button />
+  return <Calculator2 />
 }
 export default App

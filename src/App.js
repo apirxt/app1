e2bbb5a8e931081fbf4adca619fbfd9b45d6e710
apirxt <apirat.ithx@gmail.com>
2025-01-15 15:54:39 +0700

    Add file state-func.js
    Add file state-func.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
-import RefsFunc from "./refs-func";
+import MessageBox from "./state-func";
 
 function App() {
-  return <RefsFunc/>
+  return <MessageBox />
 }
 export default App
